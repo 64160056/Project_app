3cc01229939c57929c9a3f4e627725a78a5ef795
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2024-09-24 21:28:40 +0700

    Setup

--- water_tracking_app/lib/main.dart
+++ water_tracking_app/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
         ),
       ),
     );
